7aa2a94a12930e12501ae623e7119cf787351e86
Dimcho N. Nedev <dimcho.nedev@musala.com>
2016-12-16 10:36:11 +0200

    Add a logCat buffer and commands
    Add a generic buffer that can be used from multiple clients simultaneously
    Add commands for stop/start the logging and getting the new arrivals logs
    Creates the logCat files with unique names and deletes the logCat after being used

---
+++ src/main/java/com/musala/atmosphere/agent/devicewrapper/util/Buffer.java
@@ -0,0 +1,131 @@
+package com.musala.atmosphere.agent.devicewrapper.util;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.List;
+
+/**
+ * A class that holds a {@link BufferProperties} buffer for a specified key.
+ *
+ * @author dimcho.nedev
+ *
+ */
+public class Buffer<T, E> {
+    private static final int THRESHOLD = 1024;
+
+    private HashMap<T, BufferProperties<E>> logBuffer;
+
+    public Buffer() {
+        this.logBuffer = new HashMap<>();
+    }
+
+    /**
+     * Gets the new arrival part of the buffer.
+     *
+     * @param key
+     *            - the key with which a specified {@link BufferProperties} buffer is associated
+     * @return the new arrival part of the buffer
+     */
+    public List<E> getBuffer(T key) {
+        if (logBuffer.get(key) == null) {
+            return new ArrayList<E>();
+        }
+
+        List<E> requestedBuffer = null;
+        try {
+            // waits until the buffer size is changed or the operation is terminated
+            while (logBuffer.get(key).buffer.size() == logBuffer.get(key).startPosition) {
+                if (this.logBuffer.get(key) == null) {
+                    return new ArrayList<E>();
+                }
+            }
+
+            List<E> buffer = logBuffer.get(key).buffer;
+            synchronized (buffer) {
+                BufferProperties<E> properties = logBuffer.get(key);
+                int startPosition = properties.startPosition;
+                int endPosition = properties.buffer.size();
+
+                requestedBuffer = new ArrayList<E>(buffer.subList(startPosition, endPosition));
+
+                properties.startPosition = endPosition;
+
+                // clears the buffer and resets the start position
+                if (buffer.size() >= THRESHOLD) {
+                    logBuffer.get(key).buffer.clear();
+                    logBuffer.get(key).startPosition = 0;
+                }
+            }
+        } catch (NullPointerException e) {
+            return new ArrayList<E>();
+        }
+
+        return requestedBuffer;
+    }
+
+    /**
+     * Adds a buffer for a specified key.
+     *
+     * @param key
+     *            - key with which a new {@link BufferProperties} buffer will be
+     *            associated
+     */
+    public synchronized void addKey(T key) {
+        logBuffer.put(key, new BufferProperties<E>());
+    }
+
+    /**
+     * Adds a value to the {@link BufferProperties} buffer that is associated with specified key.
+     *
+     * @param key
+     *            - key with which the specified value is to be associated
+     * @param value
+     *            - value to be associated with the specified key
+     */
+    public synchronized void addValue(T key, E value) {
+        if (logBuffer.containsKey(key)) {
+            logBuffer.get(key).buffer.add(value);
+        }
+    }
+
+    /**
+     * Removes the {@link BufferProperties}} value associated with a specified
+     * key.
+     *
+     * @param key
+     *            - the key with which a specified value is associated
+     */
+    public synchronized void remove(T key) {
+        logBuffer.remove(key);
+    }
+
+    /**
+     * Returns <code>true</code> if the buffer contains a mapping for the specified key.
+     *
+     * @param key
+     *            - the key whose presence in this map is to be tested
+     * @return <code>true</code> if the buffer contains a mapping for the specified key
+     */
+    public synchronized boolean contains(T key) {
+        return logBuffer.get(key) != null;
+    }
+
+    /**
+     * A private class that contains the properties for the buffer.
+     *
+     * @author dimcho.nedev
+     *
+     */
+    private class BufferProperties<V> {
+        public int startPosition = 0;
+
+        public List<V> buffer;
+
+        public BufferProperties() {
+            this.buffer = Collections.synchronizedList(new ArrayList<V>());
+        }
+
+    }
+
+}
